fb2db1c0ffe67f21e69ff4f99e4ad7c6d708040f
Laddge <dev.laddge@gmail.com>
2022-02-05 18:11:32 +0900

    [feat] Initialize plugin

---
+++ wp-adminbar-hide-btn.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    Plugin Name: Admin Bar Hide Btn
+    Plugin URI: https://github.com/laddge/wp-adminbar-hide-btn
+    Description: Hide Admin Bar by clicking a button
+    Author: Laddge
+    Version: 1.0
+    Author URI: https://github.com/laddge
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+   exit;
+}
